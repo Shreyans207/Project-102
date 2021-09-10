c291f21345ae9aff7160d607fe434bcb15b1544c
Shreyans207 <75474973+Shreyans207@users.noreply.github.com>
2021-09-10 12:40:30 +0530

    Update chem_automation.py

--- chem_automation.py
+++ chem_automation.py
@@ -14,29 +14,29 @@ while(result) :
     element_name = input('Enter the element name : ')
     for i in range(len(list_data)) : 
         Element_No = list_data[i][0]
+        Element_Symbol = list_data[i][2]
         Element = list_data[i][1]
-        if (Element == element_name) : 
-            position = list_data[i]
-        elif (Element_No == element_name) : 
+        if (Element == element_name or Element_No == element_name or Element_Symbol == element_name) : 
             position = list_data[i]
 
     print('')
     print(f'    Atomic Number : {position[0]}')
-    print(f'    Element : {position[2]}')
-    print(f'    Symbol : {position[3]}')
-    print(f'    Atomic Mass : {position[4]}')
+    print(f'    Element : {position[1]}')
+    print(f'    Symbol : {position[2]}')
+    print(f'    Atomic Mass : {position[3]}')
     print(f'    Discoverer : {position[23]}')
     print(f'    Year : {position[24]}')
     print(f'    Type : {position[15]}')
     print('')
-    print(f'    No. Of Neutrons : {position[5]}')
-    print(f'    No. Of Protons : {position[6]}')
-    print(f'    No. Of Electrons : {position[7]}')
+    print(f'    No. Of Neutrons : {position[4]}')
+    print(f'    No. Of Protons : {position[5]}')
+    print(f'    No. Of Electrons : {position[6]}')
     print(f'    No. Of Shells : {position[26]}')
     print(f'    No. of Valence Electrons: {position[27]}')
     print('')
-    print(f'    Period : {position[8]}')
-    print(f'    Group : {position[9]}')
+    print(f'    Period : {position[7]}')
+    print(f'    Group : {position[8]}')
+    print(f'    Phase : {position[9]}')
     print(f'    Density : {position[19]}')
     print(f'    Melting Point : {position[20]}')
     print(f'    Boiling Point : {position[21]}')
@@ -45,12 +45,13 @@ while(result) :
     print(f'    Electronegativity : {position[17]}')
     print(f'    No. Of Isotopes : {position[22]}')
     print('')
-    mood = input('Wanna Continue : ')
+    mood = input('Wanna Continue (y/n) : ')
     print('')
 
-    if(mood == 'True' or mood == 'true' or mood == 'y' or mood == 't') : 
-        result = True
-    else : 
+    if(mood == 'n' or mood == 'N') : 
         result = False
+    else : 
+        result = True
+        position = None
 
 print('Thanks for using this application :)')
